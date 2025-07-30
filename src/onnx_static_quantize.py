49b1c24cc9c86a7805445e1a0cdb5816fa652e7f
Bozhi <bozhilyu@gmail.com>
2025-07-30 17:26:58 +0200

    static quantize test-part of nodes.

--- src/onnx_static_quantize.py
+++ src/onnx_static_quantize.py
@@ -3,6 +3,7 @@ import torch
 from transformers import AutoFeatureExtractor
 import numpy as np
 import yaml
+import onnx
 import argparse
 
 from onnxruntime.quantization import QuantFormat, quantize_static, CalibrationDataReader
@@ -38,9 +39,20 @@ if __name__ == "__main__":
         config = yaml.safe_load(f)
     _, _, validate_loader = get_data_loaders(config["data"])
 
+    onnx_static = onnx.load("./models/wav2vec2_finetuned.onnx")
+    matmul_nodes = [node for node in onnx_static.graph.node if node.op_type == "MatMul"]
+    conv_nodes = [node for node in onnx_static.graph.node if node.op_type == "Conv"]
+    Gemm_nodes = [node for node in onnx_static.graph.node if node.op_type == "Gemm"]
+    Add_nodes = [node for node in onnx_static.graph.node if node.op_type == "Add"]
+    Mul_nodes = [node for node in onnx_static.graph.node if node.op_type == "Mul"]
+    Div_nodes = [node for node in onnx_static.graph.node if node.op_type == "Div"]
+    ReduceMean_nodes = [node for node in onnx_static.graph.node if node.op_type == "ReduceMean"]
+    nodes_to_quantize = matmul_nodes + conv_nodes + Gemm_nodes + Add_nodes + Mul_nodes + Div_nodes + ReduceMean_nodes
+
     quantize_static(
         model_input=args.input,
         model_output=args.output,
+        nodes_to_quantize=nodes_to_quantize,
         calibration_data_reader=DataReader(validate_loader),
         quant_format=QuantFormat.QDQ, # Models quantized by quantize_static are in QDQ format
         per_channel=args.per_channel,
