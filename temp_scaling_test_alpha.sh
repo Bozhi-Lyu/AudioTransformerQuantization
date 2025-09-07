b966c6fd3773f9893531c8b90dbf0f4eb9b23e2f
Bozhi <bozhilyu@gmail.com>
2025-09-07 21:26:53 +0200

    optimize for job array.

--- temp_scaling_test_alpha.sh
+++ temp_scaling_test_alpha.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#BSUB -J audioml_onnx_dynamic_quantize
+#BSUB -J temperature_alpha_test[1-5]
 #BSUB -n 4
 #BSUB -q gpuv100
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 12:00
-#BSUB -R "rusage[mem=8GB]"
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -R "span[hosts=1]"
 #BSUB -B
 #BSUB -N
-#BSUB -o audioml_onnx_dynamic_quantize_%J.out
-#BSUB -e audioml_onnx_dynamic_quantize_%J.err
+#BSUB -o alpha%J.out
+#BSUB -e alpha%J.err
 
 module unload python3
 source /dtu/projects/02613_2025/conda/conda_init.sh
@@ -20,8 +21,13 @@ conda activate audioml
 
 python -m pip install -e . -q
 
-python src/mix_precision_QAT.py --alpha 0.1
-python src/mix_precision_QAT.py --alpha 0.3
-python src/mix_precision_QAT.py --alpha 0.5
-python src/mix_precision_QAT.py --alpha 0.7
-python src/mix_precision_QAT.py --alpha 0.9
+case $LSB_JOBINDEX in
+    1) ALPHA=0.1 ;;
+    2) ALPHA=0.3 ;;
+    3) ALPHA=0.5 ;;
+    4) ALPHA=0.7 ;;
+    5) ALPHA=0.9 ;;
+esac
+
+echo "Running with alpha=$ALPHA"
+python src/mix_precision_QAT.py --alpha $ALPHA
